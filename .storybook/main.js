b54ecbd5e0c81705680f63caa98e4efad6890833
Greg Ogun <gregogun97@gmail.com>
2021-11-25 18:41:17 +0000

    feat: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "core": {
+    "builder": "webpack5"
+  },
+  // code below fixes a common issue with storybook still trying to reference webpack4
+  typescript: { reactDocgen: false },
+}
