989e661a1a1db7ceb901713e0867f0c0363dc031
Greg Ogun <gregogun97@gmail.com>
2021-11-25 18:42:53 +0000

    feat: add export

---
+++ index.ts
@@ -0,0 +1,26 @@
+export { Box } from './components/Box'
+export type { BoxProps } from './components/Box'
+
+export { Flex } from './components/Flex'
+export type { FlexProps } from './components/Flex'
+
+export { Grid } from './components/Grid'
+export type { GridProps } from './components/Grid'
+
+export { Center } from './components/Center'
+export type { CenterProps } from './components/Center'
+
+export { Text } from './components/Text'
+export type { TextProps, TextVariants } from './components/Text'
+
+export { Heading } from './components/Heading'
+export type { HeadingProps, HeadingVariants } from './components/Heading'
+
+export * from './stitches.config'
+export type {
+  ThemeCSS,
+  ComponentProps,
+  PropertyValue,
+  ScaleValue,
+  VariantProps,
+} from './stitches.config'
