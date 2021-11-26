ae8d44b6de6494275baf8719f044bb0997953e7d
Greg Ogun <gregogun97@gmail.com>
2021-11-26 19:09:33 +0000

    feat: add stories for heading component

---
+++ components/Heading/Heading.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Heading } from './index'
+
+export default {
+  title: 'Components/Heading',
+  component: Heading,
+} as ComponentMeta<typeof Heading>
+
+export const Heading6xl: React.VFC<{}> = () => (
+  <Heading size="6xl">Welcome to zerogravity</Heading>
+)
+
+export const Heading5xl: React.VFC<{}> = () => (
+  <Heading size="5xl">Welcome to zerogravity</Heading>
+)
+
+export const Heading4xl: React.VFC<{}> = () => (
+  <Heading size="4xl">Welcome to zerogravity</Heading>
+)
+
+export const Heading3xl: React.VFC<{}> = () => (
+  <Heading size="3xl">Welcome to zerogravity</Heading>
+)
+
+export const Heading2xl: React.VFC<{}> = () => (
+  <Heading size="2xl">Welcome to zerogravity</Heading>
+)
+
+export const HeadingXl: React.VFC<{}> = () => (
+  <Heading size="xl">Welcome to zerogravity</Heading>
+)
+
+export const HeadingLg: React.VFC<{}> = () => (
+  <Heading size="lg">Welcome to zerogravity</Heading>
+)
+
+export const HeadingMd: React.VFC<{}> = () => (
+  <Heading size="md">Welcome to zerogravity</Heading>
+)
+
+export const HeadingSm: React.VFC<{}> = () => (
+  <Heading size="sm">Welcome to zerogravity</Heading>
+)
+
+export const HeadingXs: React.VFC<{}> = () => (
+  <Heading size="xs">Welcome to zerogravity</Heading>
+)
