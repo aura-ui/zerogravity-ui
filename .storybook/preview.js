3e6bb8594252195eb01e680952114c3f3ee485c4
Greg Ogun <gregogun97@gmail.com>
2021-11-28 19:46:31 +0000

    feat: add theme toggle for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,22 +1,37 @@
 import '../styles/globals.css'
-import { darkTheme } from '../stitches.config';
-import { ThemeProvider } from 'next-themes';
+import { darkTheme } from '../stitches.config'
+import { ThemeProvider } from 'next-themes'
+import { Flex } from '../components/Flex'
+import { Text } from '../components/Text'
+import { ThemeToggle } from '../utils/components/ThemeToggle'
 
 export const decorators = [
   (Story) => (
     <ThemeProvider
       disableTransitionOnChange
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       value={{ light: 'light-theme', dark: darkTheme.toString() }}
     >
-        <Story />
+      <Flex
+        css={{
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          p: '$2',
+          mb: '$6',
+          boxShadow: '0 1px 1px $colors$subtleBorder',
+        }}
+      >
+        <Text>zerogravity</Text>
+        <ThemeToggle />
+      </Flex>
+      <Story />
     </ThemeProvider>
-  )
-];
+  ),
+]
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
