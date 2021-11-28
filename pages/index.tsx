8afbea154fe365d73c70edd388fa34c20b85a1e8
Greg Ogun <gregogun97@gmail.com>
2021-11-28 23:59:04 +0000

    feat: add theme switch to nextjs page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,43 @@ import { Center } from '../components/Center'
 import { Heading } from '../components/Heading'
 import { Button } from '../components/Button'
 import { AutoLayout } from '../components/AutoLayout'
+import { Flex } from '../components/Flex'
+import { Box } from '../components/Box'
+import { Text } from '../components/Text'
+import { ThemeToggle } from '../utils/components/ThemeToggle'
 
 const Home: NextPage = () => {
   return (
-    <Center>
-      <Heading>Welcome to zerogravity</Heading>
-      <AutoLayout css={{ gap: '$4' }}>
-        <Button size="sm">Log In</Button>
-        <Button variant="outline" size="md">
-          Log In
-        </Button>
-        <Button variant="solid" size="md">
-          Log In
-        </Button>
-        <Button variant="ghost" size="lg">
-          Log In
-        </Button>
-      </AutoLayout>
-    </Center>
+    <Box>
+      <Flex
+        css={{
+          justifyContent: 'space-between',
+          p: '$2',
+          mb: '$6',
+          boxShadow: '0 1px 1px $colors$subtleBorder',
+        }}
+      >
+        <Text>zerogravity</Text>
+        <ThemeToggle />
+      </Flex>
+      <Center>
+        <AutoLayout>
+          <Heading>Welcome to zerogravity</Heading>
+          <AutoLayout css={{ gap: '$4' }}>
+            <Button size="sm">Log In</Button>
+            <Button variant="outline" size="md">
+              Log In
+            </Button>
+            <Button variant="solid" size="md">
+              Log In
+            </Button>
+            <Button variant="ghost" size="lg">
+              Log In
+            </Button>
+          </AutoLayout>
+        </AutoLayout>
+      </Center>
+    </Box>
   )
 }
 
